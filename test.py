24af4848765386f2e032d5d697b3e74601473ac7
Eric Fourrier <ericfourrier0@gmail.com>
2015-04-27 02:32:48 -0700

    Add new tests

--- test.py
+++ test.py
@@ -15,10 +15,10 @@ The clock decorator in utils will measure the run time of the test
 
 import unittest
 # internal helpers
-from decam.utils import create_test_df
-from decam.utils import clock 
-from decam.modeling_helpers import DataCleaner
+from decam.utils import *
+from decam.modeling_helpers import *
 import pandas as pd
+import numpy as np 
 
 
 flatten_list = lambda x: [y for l in x for y in flatten_list(l)] if isinstance(x, list) else [x]
@@ -31,12 +31,24 @@ cserie = lambda serie: list(serie[serie].index)
 
 
 
-class TestPandasPatch(unittest.TestCase):
+class TestDataCleaner(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         """ creating test data set for the test module """
         cls._test_dc = DataCleaner(data = create_test_df())
+
+    @clock
+    def test_cserie(self):
+        char_var = cserie(self._test_dc.data.dtypes == "object")
+        self.assertIsInstance(char_var,list)
+        self.assertIn('character_variable',char_var)
+
+    @clock
+    def test_removena_numpy(self):
+        test_array = np.array([np.nan,1,2,np.nan])
+        self.assertTrue((removena_numpy(test_array) == np.array([1,2])).all())
+
 
     @clock
     def test_sample_df(self):
@@ -74,12 +86,14 @@ class TestPandasPatch(unittest.TestCase):
     @clock
     def test_manymissing_capture(self):
         manymissing = self._test_dc.manymissing(0.7)
+        self.assertIsInstance(manymissing,list)
         self.assertIn('many_missing_70', manymissing)
         self.assertIn('na_col', manymissing)
 
     @clock
     def test_constant_col_capture(self):
         constantcol = self._test_dc.constantcol()
+        self.assertIsInstance(constantcol,list)
         self.assertIn('constant_col', constantcol)
         self.assertIn('constant_col_num', constantcol)
         self.assertIn('na_col', constantcol)
@@ -87,6 +101,7 @@ class TestPandasPatch(unittest.TestCase):
     @clock
     def test_count_unique(self):
         count_unique = self._test_dc.count_unique()
+        self.assertIsInstance(count_unique,pd.Series)
         self.assertEqual(count_unique.id, 1000)
         self.assertEqual(count_unique.constant_col, 1)
         self.assertEqual(count_unique.character_factor, 7)
@@ -120,6 +135,7 @@ class TestPandasPatch(unittest.TestCase):
     @clock
     def test_detectkey_check_col(self):
         detectkey = self._test_dc.detectkey()
+        self.assertIsInstance(detectkey,list)
         self.assertIn('id', detectkey)
         self.assertIn('member_id', detectkey)
 
@@ -166,6 +182,47 @@ class TestPandasPatch(unittest.TestCase):
         self.assertIn('nearzerovar_variable', cserie(nearzerovar.nzv))
         self.assertIn('constant_col', cserie(nearzerovar.nzv))
         self.assertIn('na_col', cserie(nearzerovar.nzv))
+
+    @clock
+    def test_fillna_serie(self):
+        test_char_variable = self._test_dc.fillna_serie(self._test_dc.data.character_variable_fillna)
+        test_num_variable = self._test_dc.fillna_serie(self._test_dc.data.numeric_variable_fillna)
+        self.assertTrue(test_char_variable.notnull().any())
+        self.assertTrue(test_num_variable.notnull().any())
+        self.assertTrue((pd.Series(['A']*300 + ['B']*200 + ['C']*200 +['A']*300) == test_char_variable).all())
+        self.assertTrue((pd.Series([1]*400 + [3]*400 + [2]*200) == test_num_variable).all())
+
+    @clock
+    def test_fill_low_na(self):
+        df_fill_low_na = self._test_dc.fill_low_na(columns_to_process = ['character_variable_fillna',
+            'numeric_variable_fillna'])
+        df_fill_low_na_threshold = self._test_dc.fill_low_na(threshold = 0.4)
+        self.assertIsInstance(df_fill_low_na,pd.DataFrame)
+        self.assertIsInstance(df_fill_low_na_threshold,pd.DataFrame)
+        self.assertTrue((pd.Series(['A']*300 + ['B']*200 + ['C']*200 +['A']*300) == df_fill_low_na.character_variable_fillna).all())
+        self.assertTrue((pd.Series([1]*400 + [3]*400 + [2]*200) == df_fill_low_na.numeric_variable_fillna).all())
+        self.assertTrue((pd.Series(['A']*300 + ['B']*200 + ['C']*200 +['A']*300) == df_fill_low_na_threshold.character_variable_fillna).all())
+        self.assertTrue((pd.Series([1]*400 + [3]*400 + [2]*200) == df_fill_low_na_threshold.numeric_variable_fillna).all())
+        self.assertTrue(sum(pd.isnull(df_fill_low_na_threshold.many_missing_70)) == 700)
+
+    @clock
+    def test_to_dummy(self):
+        df_dummy_subset = self._test_dc.to_dummy(auto = False,
+            subset = ['character_variable_fillna','character_factor'],verbose = False)
+        self.assertIsInstance(df_dummy_subset,pd.DataFrame)
+        for e in list('ABCDEFG'):
+            self.assertIn('character_factor_' + e,df_dummy_subset.columns)
+        for e in list('ABC'):
+            self.assertIn('character_variable_fillna_' + e,df_dummy_subset.columns)
+        self.assertIn('character_variable_fillna_nan',df_dummy_subset.columns)
+
+
+
+
+
+
+
+
 
 
 # Adding new tests sets
